151a4d353c1fdb08dd6b24f3a8a2d844dbf0e0f6
Michel Marti <mma@objectxp.com>
2012-08-01 10:39:58 +0200

    ECM Sim: Verify checksums, display SET Req parameters

--- ecmsim/src/org/ecmdroid/sim/Main.java
+++ ecmsim/src/org/ecmdroid/sim/Main.java
@@ -40,7 +40,8 @@ import org.ecmdroid.PDU;
 public class Main
 {
 	static final int PORT = 6275;
-	static final PDU ACK = new PDU(PDU.ECM_ID, PDU.DROID_ID, new byte[]{0x06, 0x00});
+	static final PDU ACK  = new PDU(PDU.ECM_ID, PDU.DROID_ID, new byte[]{0x06, 0x00});
+	static final PDU NACK = new PDU(PDU.ECM_ID, PDU.DROID_ID, new byte[]{0x42, 0x42});
 	static final byte[]  VERSION_RESPONSE = {0x01, 0x42, 0x00, 0x13, (byte) 0xff, 0x02, 0x06, 0x42, 0x55, 0x45, 0x32, 0x44, 0x32, 0x34, 0x32, 0x20, 0x31, 0x31, 0x2d, 0x33, 0x30, 0x2d, 0x30, 0x39, 0x03, (byte) 0x93};
 	static byte[] RT_DATA = new byte[] {0x01, 0x42, 0x00, (byte) 0x80, (byte) 0xff, 0x02, 0x06, 0x15, 0x58, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0xcc, 0x25, (byte) 0xcc, 0x25, 0x13, 0x3f, 0x13, 0x3f, 0x23, 0x00, 0x15, (byte) 0xb5, 0x04, 0x49, 0x02, 0x7e, 0x02, 0x66, 0x00, 0x58, 0x02, 0x37, 0x06, (byte) 0xf2, 0x03, 0x00, 0x00, (byte) 0xe8, 0x03, (byte) 0xe8, 0x03, (byte) 0xe8, 0x03, (byte) 0xe8, 0x03, (byte) 0xe8, 0x03, (byte) 0x86, 0x03, 0x00, (byte) 0x90, 0x00, 0x11, 0x3c, (byte) 0xb1, 0x5c, (byte) 0xff, 0x03, (byte) 0xe4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x00, 0x7d, 0x00, 0x00, 0x00, 0x00, 0x56, 0x00, 0x34, 0x01, (byte) 0xf2, (byte) 0xc2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0x83, 0x67, (byte) 0xff, (byte) 0xff, 0x70, 0x19, (byte) 0xff, (byte) 0xff, (byte) 0xff, 0x4d, 0x00, (byte) 0xdc, 0x00, 0x00, (byte) 0xb1, 0x5c, (byte) 0xff, (byte) 0xff, 0x00, 0x00, 0x32, 0x00, 0x64, (byte) 0xe8, 0x03, (byte) 0xe8, 0x03, 0x00, 0x03, 0x7c};
 
@@ -68,6 +69,17 @@ public class Main
 					}
 					len = (buffer[3]&0xff) + 1;
 					len = in.read(buffer, 6, len);
+					// Verify checksum
+					byte cs = 0;
+					for (int i = 1; i < len + 5; i++) {
+						cs ^= (buffer[i]);
+					}
+					if (buffer[len+5] != cs) {
+						System.err.println("Checksum failed.");
+						out.write(NACK.getBytes());
+						continue;
+					}
+
 					// Thread.sleep(50);
 					switch (buffer[6])
 					{
@@ -76,7 +88,10 @@ public class Main
 						out.write(VERSION_RESPONSE);
 						break;
 					case PDU.CMD_SET:
-						System.out.println("SET Command");
+						int offset = buffer[7]  & 0xff;
+						int pgno = buffer[8] & 0xff;
+						len = buffer[9] & 0xff;
+						System.out.println("SET Command, page " + pgno +", offset " + offset + ", len " + len);
 						// Page number 32 -> Virtual Page
 						if (buffer[8] == 0x20) {
 							System.out.println("Device test");
@@ -95,15 +110,15 @@ public class Main
 						break;
 
 					case PDU.CMD_GET:
-						int pgno = buffer[8] & 0xff;
-						int offset  = buffer[7] & 0xff;
-						int length = buffer[9] & 0xff;
-						System.out.println("GET request for " + length  + " bytes from page " + pgno + " at offset " + offset);
-						byte[] payload = new byte[length+1];
+						pgno = buffer[8] & 0xff;
+						offset  = buffer[7] & 0xff;
+						len = buffer[9] & 0xff;
+						System.out.println("GET request for " + len  + " bytes from page " + pgno + " at offset " + offset);
+						byte[] payload = new byte[len+1];
 						payload[0]=0x06;
 						if (pgno != 32) {
 							Page pg = eeprom.getPage(pgno);
-							pg.getBytes(offset, length, payload, 1);
+							pg.getBytes(offset, len, payload, 1);
 						} else {
 							// Virtual page 0x20 - device tests
 							byte stat = (byte) ((System.currentTimeMillis() - deviceTestStart > 3000) ? 0 : 1);
@@ -114,8 +129,7 @@ public class Main
 						break;
 					default:
 						System.out.println("Unsupported command -> NACK");
-						PDU pdu = new PDU(PDU.ECM_ID, PDU.DROID_ID, new byte[]{0x42, 0x42});
-						out.write(pdu.getBytes());
+						out.write(NACK.getBytes());
 					}
 				} catch (SocketException se) {
 					s.close();
