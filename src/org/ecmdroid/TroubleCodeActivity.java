d8697635e93d2cd8e0b4c11b26f4dfcf880b6f84
Michel Marti <mma@objectxp.com>
2012-08-03 17:10:14 +0200

    Trouble Codes: Refresh on resume, Simplify AsyncTasks

--- src/org/ecmdroid/TroubleCodeActivity.java
+++ src/org/ecmdroid/TroubleCodeActivity.java
@@ -22,20 +22,19 @@ import java.util.Collection;
 
 import org.ecmdroid.Error.ErrorType;
 import org.ecmdroid.PDU.Function;
+import org.ecmdroid.tasks.ProgressDialogTask;
 
 import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.pm.ActivityInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class TroubleCodeActivity extends BaseActivity implements OnClickListener {
+	private Collection<Error> currentErrors;
+	private Collection<Error> storedErrors;
 	private ECM ecm = ECM.getInstance(this);
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,26 +48,17 @@ public class TroubleCodeActivity extends BaseActivity implements OnClickListener
 		super.onResume();
 		((Button)findViewById(R.id.readErrors)).setEnabled(ecm.isConnected());
 		((Button)findViewById(R.id.clearErrors)).setEnabled(ecm.isConnected());
+		update();
 	}
-
 
 	public void onClick(View view) {
 		if (view.getId() == R.id.readErrors) {
 			// Fetch runtime data and update fields
-			new AsyncTask<Void, Void, Exception>() {
-				private ProgressDialog pd;
-				private Collection<Error> currentErrors;
-				private Collection<Error> storedErrors;
-				private int ro;
+			new ProgressDialogTask(TroubleCodeActivity.this, "") {
+
 				@Override
-				protected void onPreExecute() {
-					// Prevent screen rotation during progress dialog display
-					ro = getRequestedOrientation();
-					setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
-					pd = ProgressDialog.show(TroubleCodeActivity.this, "", getString(R.string.fetching_trouble_codes), true);
-				};
-				@Override
-				protected Exception doInBackground(Void... args) {
+				protected Exception doInBackground(Void... params) {
+					publishProgress(TroubleCodeActivity.this.getString(R.string.fetching_trouble_codes));
 					try {
 						ecm.readRTData();
 						currentErrors = ecm.getErrors(ErrorType.CURRENT);
@@ -80,28 +70,9 @@ public class TroubleCodeActivity extends BaseActivity implements OnClickListener
 				}
 				@Override
 				protected void onPostExecute(Exception result) {
-					pd.dismiss();
-					setRequestedOrientation(ro);
-					if (result != null) {
-						Toast.makeText(TroubleCodeActivity.this, "Error fetching trouble codes. " + result.getMessage(), Toast.LENGTH_LONG).show();
-						return;
-					}
-					EditText ce = (EditText) findViewById(R.id.currentErrors);
-					ce.setText(errors2str(currentErrors));
-					EditText se = (EditText) findViewById(R.id.storedErrors);
-					se.setText(errors2str(storedErrors));
+					super.onPostExecute(result);
+					update();
 				}
-				private CharSequence errors2str(Collection<Error> errors) {
-					if (errors == null || errors.size() == 0) {
-						return getText(R.string.no_errors);
-					}
-					String fmt = "%3s | %s\n";
-					StringBuffer sb = new StringBuffer();
-					for (Error e : errors) {
-						sb.append(String.format(fmt, e.getCode(), e.getDescription()));
-					}
-					return sb;
-				};
 			}.execute();
 		}
 		else if (view.getId() == R.id.clearErrors) {
@@ -110,41 +81,59 @@ public class TroubleCodeActivity extends BaseActivity implements OnClickListener
 				public void onClick(DialogInterface alert, int arg) {
 					alert.cancel();
 				}
-			}).create();
-			alert.setButton(getText(android.R.string.ok), new DialogInterface.OnClickListener() {
+			}).setPositiveButton(getText(android.R.string.ok), new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface alert, int arg) {
 					alert.dismiss();
-					new AsyncTask<Void, Void, Exception>() {
-						private ProgressDialog pd;
-						private int ro;
-						@Override
-						protected void onPreExecute() {
-							// Prevent screen rotation during progress dialog display
-							ro = getRequestedOrientation();
-							setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
-							pd = ProgressDialog.show(TroubleCodeActivity.this, "", getString(R.string.clearing_trouble_codes), true);
-						}
+					new ProgressDialogTask(TroubleCodeActivity.this, ""){
 						@Override
 						protected Exception doInBackground(Void... args) {
+							publishProgress(TroubleCodeActivity.this.getString(R.string.clearing_trouble_codes));
 							try {
 								ecm.runTest(Function.ClearCodes);
+								ecm.readRTData();
 							} catch (IOException e) {
 								return e;
 							}
 							return null;
 						}
+
 						@Override
 						protected void onPostExecute(Exception result) {
-							pd.dismiss();
-							setRequestedOrientation(ro);
-							if (result != null) {
-								Toast.makeText(TroubleCodeActivity.this, "I/O error clearing codes.", Toast.LENGTH_LONG).show();
-							}
-						}
+							super.onPostExecute(result);
+							update();
+						};
 					}.execute();
 				}
-			});
+			}).create();
 			alert.show();
 		}
 	}
+
+	private void update() {
+		if (ecm.getRealtimeData() != null) {
+			try {
+				currentErrors = ecm.getErrors(ErrorType.CURRENT);
+				storedErrors = ecm.getErrors(ErrorType.STORED);
+			} catch (IOException e) {}
+		}
+		EditText ce = (EditText) findViewById(R.id.currentErrors);
+		ce.setText(errors2str(currentErrors));
+		EditText se = (EditText) findViewById(R.id.storedErrors);
+		se.setText(errors2str(storedErrors));
+	}
+
+	private CharSequence errors2str(Collection<Error> errors) {
+		if (errors == null) {
+			return "";
+		}
+		if (errors.size() == 0) {
+			return getText(R.string.no_errors);
+		}
+		String fmt = "%3s | %s\n";
+		StringBuffer sb = new StringBuffer();
+		for (Error e : errors) {
+			sb.append(String.format(fmt, e.getCode(), e.getDescription()));
+		}
+		return sb;
+	};
 }
