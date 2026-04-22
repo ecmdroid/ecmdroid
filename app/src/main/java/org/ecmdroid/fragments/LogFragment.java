ecmdroid/ecmdroid

commit d60ef87b05e2902ea8def2a04dc1a9c897a50489

ecmdroid 0.99.5: use Document Framework API for file-storage
Newer Android versions prevent users from accessing the app-specific
storage. Replace the log- and eeprom storage access framework instead.

For the log recorder, you must select the desired storage location once.
Existing eeprom dumps and log files will *not* be migrated to the new
storage framework.

Also update min- and target-sdk version to 26/33 (play store now requires
apps to target Android 13 or higher). Bump some dependencies and update
gradle to v8.4.

While at it: change checkboxes in prefs to switches and tweak the "log"
layout a bit.

app/src/main/java/org/ecmdroid/fragments/LogFragment.java
@@ -27,10 +27,13 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.ParcelFileDescriptor;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v4.provider.DocumentFile;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -39,8 +42,8 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,18 +51,15 @@ import org.ecmdroid.Constants.Variables;
 import org.ecmdroid.ECM;
 import org.ecmdroid.EcmDroidService;
 import org.ecmdroid.R;
-import org.ecmdroid.Utils;
 import org.ecmdroid.Variable;
 import org.ecmdroid.activities.MainActivity;
+import org.ecmdroid.activities.PrefsActivity;
 import org.ecmdroid.task.ProgressDialogTask;
 import org.ecmdroid.util.Bin2MslConverter;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -69,11 +69,13 @@ public class LogFragment extends Fragment implements OnClickListener {
 	private static final String PREFS_KEEP_SCREEN_ON = "keep_screen_on";
 	private static final String TAG = "LogFragment";
 	private Button recordButton;
-	private TextView logFile;
 	private TextView logStatus;
 	private TextView tpsValue, rpmValue, cltValue;
 	private ECM ecm = ECM.getInstance(getActivity());
 	private EcmDroidService ecmDroidService;
+	private static DocumentFile docRoot;
+	private static ParcelFileDescriptor logFile;
+	private static CharSequence logTimestamp;
 
 	private static class Interval {
 		int delay;
@@ -131,7 +133,6 @@ public class LogFragment extends Fragment implements OnClickListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.log, container, false);
-		logFile = (TextView) view.findViewById(R.id.logFileValue);
 		logStatus = (TextView) view.findViewById(R.id.logStatusValue);
 		tpsValue = (TextView) view.findViewById(R.id.tpsValue);
 		rpmValue = (TextView) view.findViewById(R.id.rpmValue);
@@ -158,18 +159,14 @@ public class LogFragment extends Fragment implements OnClickListener {
 		MainActivity activity = (MainActivity) getActivity();
 		activity.updateConnectButton();
 		activity.setTitle(getString(R.string.log_recorder));
-		activity.registerReceiver(receiver, new IntentFilter(EcmDroidService.RECORDING_STARTED));
-		activity.registerReceiver(receiver, new IntentFilter(EcmDroidService.RECORDING_STOPPED));
-		activity.registerReceiver(receiver, new IntentFilter(EcmDroidService.REALTIME_DATA));
-		Spinner spinner = (Spinner) getView().findViewById(R.id.logInterval);
-		if (ecmDroidService != null && ecm.isRecording()) {
-			spinner.setEnabled(false);
-		} else {
-			spinner.setEnabled(true);
-		}
+		activity.registerReceiver(receiver, new IntentFilter(EcmDroidService.RECORDING_STARTED), Context.RECEIVER_NOT_EXPORTED);
+		activity.registerReceiver(receiver, new IntentFilter(EcmDroidService.RECORDING_STOPPED), Context.RECEIVER_NOT_EXPORTED);
+		activity.registerReceiver(receiver, new IntentFilter(EcmDroidService.REALTIME_DATA), Context.RECEIVER_NOT_EXPORTED);
+		Spinner spinner = getView().findViewById(R.id.logInterval);
+		spinner.setEnabled(ecmDroidService == null || !ecm.isRecording());
 		SharedPreferences prefs = activity.getPreferences(Activity.MODE_PRIVATE);
 		spinner.setSelection(prefs.getInt(PREFS_DELAY, 0));
-		CheckBox convert = (CheckBox) getView().findViewById(R.id.logConvertCheckbox);
+		Switch convert = getView().findViewById(R.id.logConvertCheckbox);
 		convert.setChecked(prefs.getBoolean(PREFS_CONVERTLOG, false));
 	}
 
@@ -178,7 +175,7 @@ public class LogFragment extends Fragment implements OnClickListener {
 		super.onPause();
 		SharedPreferences prefs = getActivity().getPreferences(Activity.MODE_PRIVATE);
 		Spinner spinner = (Spinner) getView().findViewById(R.id.logInterval);
-		CheckBox convert = (CheckBox) getView().findViewById(R.id.logConvertCheckbox);
+		Switch convert = (Switch) getView().findViewById(R.id.logConvertCheckbox);
 		Editor editor = prefs.edit();
 		editor.putInt(PREFS_DELAY, spinner.getSelectedItemPosition());
 		editor.putBoolean(PREFS_CONVERTLOG, convert.isChecked());
@@ -205,7 +202,7 @@ public class LogFragment extends Fragment implements OnClickListener {
 					Toast.makeText(getActivity(), "I/O error. " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
 				}
 			} else {
-				boolean convert = ((CheckBox) getView().findViewById(R.id.logConvertCheckbox)).isChecked();
+				boolean convert = ((Switch) getView().findViewById(R.id.logConvertCheckbox)).isChecked();
 				recordButton.setEnabled(false);
 				new StopTask(getActivity(), convert).execute();
 				recordButton.setEnabled(true);
@@ -216,31 +213,34 @@ public class LogFragment extends Fragment implements OnClickListener {
 	}
 
 	private void startRecording() throws IOException {
-		if (!Utils.isExternalStorageAvailable()) {
-			Toast.makeText(getActivity(), R.string.no_ext_storage, Toast.LENGTH_LONG).show();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		String uri = prefs.getString("storage.location", null);
+		if (uri == null) {
+			Toast.makeText(getActivity(), R.string.choose_storage_location, Toast.LENGTH_LONG).show();
+			Intent intent = new Intent(this.getActivity(), PrefsActivity.class);
+			startActivity(intent);
 			return;
 		}
-		Spinner spinner = (Spinner) getView().findViewById(R.id.logInterval);
+		Spinner spinner = getView().findViewById(R.id.logInterval);
 		Interval interval = (Interval) spinner.getSelectedItem();
-
-		CharSequence fn = DateFormat.format("yyyyMMdd_kkmmss", System.currentTimeMillis());
-		File dir = getActivity().getApplication().getExternalFilesDir(getString(R.string.log_dir));
-		if (!dir.exists() && !dir.mkdirs()) {
-			Log.w(TAG, "Unable to create directories " + dir.getAbsolutePath());
-		}
 		recordButton.setText(R.string.stop_recording);
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+		docRoot = DocumentFile.fromTreeUri(getContext(), Uri.parse(uri));
+		logTimestamp = DateFormat.format("yyyyMMdd_kkmmss", System.currentTimeMillis());
+		DocumentFile f = docRoot.createFile("application/x-ecmdroid-binlog", logTimestamp.toString() + ".log");
+		logFile = getContext().getContentResolver().openFileDescriptor(f.getUri(), "rw");
+		FileOutputStream out = new FileOutputStream(logFile.getFileDescriptor());
+
 		if (prefs.getBoolean(PREFS_KEEP_SCREEN_ON, false)) {
 			Log.i(TAG, "Keeping Screen on while recording...");
 			getView().setKeepScreenOn(true);
 		}
-		ecmDroidService.startRecording(new File(dir, fn + ".bin"), interval == null ? 0 : interval.delay, ecm);
+		ecmDroidService.startRecording(out, interval.delay, ecm);
 	}
 
 	protected void updateUI() {
 		if (ecmDroidService != null) {
 			if (ecm.isRecording()) {
-				logFile.setText(ecmDroidService.getLogfile());
 				logStatus.setText(String.format(getString(R.string.log_status), ecmDroidService.getRecords(), ecmDroidService.getBytes() / 1024));
 				Variable tps = ecm.getRuntimeValue(Variables.TPD);
 				Variable rpm = ecm.getRuntimeValue(Variables.RPM);
@@ -255,7 +255,6 @@ public class LogFragment extends Fragment implements OnClickListener {
 					cltValue.setText(clt.getFormattedValue());
 				}
 			} else {
-				logFile.setText(R.string.dash);
 				logStatus.setText(R.string.status_idle);
 				tpsValue.setText(R.string.dash);
 				rpmValue.setText(R.string.dash);
@@ -278,43 +277,67 @@ public class LogFragment extends Fragment implements OnClickListener {
 		@Override
 		protected Exception doInBackground(Void... params) {
 			Exception ret = null;
-			File log = ecmDroidService.getCurrentFile();
 			publishProgress(context.getString(R.string.stopping_logfile_recorder));
 			ecmDroidService.stopRecording();
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 			}
-			if (convert) {
-				setCancelable(true);
-				publishProgress(context.getString(R.string.converting_to_msl));
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
+			try {
+				if (convert) {
+					ret = convertToMSL();
 				}
-				InputStream in = null;
-				OutputStream out = null;
+			} finally {
 				try {
-					in = new FileInputStream(log);
-					out = new FileOutputStream(new File(log.getAbsolutePath().replaceAll("bin$", "msl")));
-					converter.addObserver(this);
-					converter.convert(in, out);
-				} catch (Exception e) {
-					Log.w(TAG, "Conversion failed.", e);
-					ret = new Exception(context.getString(R.string.conversion_failed) + " " + e.getMessage());
-				} finally {
-					try {
-						if (in != null)
-							in.close();
-
-						if (out != null) {
-							out.flush();
-							out.close();
-						}
-					} catch (Exception e) {
-						Log.w(TAG, "Unable to close Input/Output stream.", e);
-						ret = e;
+					if (logFile != null) {
+						logFile.close();
 					}
+				} catch (IOException e) {
+					ret = e;
+				}
+				logFile = null;
+			}
+			return ret;
+		}
+
+		private Exception convertToMSL() {
+			Exception ret = null;
+			setCancelable(true);
+			publishProgress(context.getString(R.string.converting_to_msl));
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				Log.d(TAG, "Sleep interrupted");
+			}
+			FileInputStream in = null;
+			FileOutputStream out = null;
+			ParcelFileDescriptor mslFile = null;
+			try {
+				DocumentFile f = docRoot.createFile("text/plain", logTimestamp.toString() + ".msl");
+				mslFile = getContext().getContentResolver().openFileDescriptor(f.getUri(), "rw");
+				in = new FileInputStream(logFile.getFileDescriptor());
+				in.getChannel().position(0);
+				out = new FileOutputStream(mslFile.getFileDescriptor());
+				converter.addObserver(this);
+				converter.convert(in, out);
+			} catch (Exception e) {
+				Log.w(TAG, "Conversion failed.", e);
+				ret = new Exception(context.getString(R.string.conversion_failed) + " " + e.getMessage());
+			} finally {
+				try {
+					if (in != null)
+						in.close();
+
+					if (out != null) {
+						out.flush();
+						out.close();
+					}
+					if (mslFile != null) {
+						mslFile.close();
+					}
+				} catch (Exception e) {
+					Log.w(TAG, "Unable to close Input/Output stream.", e);
+					ret = e;
 				}
 			}
 			return ret;

app/build.gradle
@@ -1,11 +1,12 @@
-apply plugin: 'com.android.application'
+plugins {
+    alias(libs.plugins.androidApplication)
+}
 
 def keystorePropertiesFile = rootProject.file("keystore.properties")
 def keystoreProperties = new Properties()
 keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
 
 android {
-    compileSdkVersion 30
     signingConfigs {
         config {
             keyAlias keystoreProperties['keyAlias']
@@ -15,11 +16,12 @@ android {
         }
     }
     defaultConfig {
+        compileSdk 34
         applicationId "org.ecmdroid"
-        versionCode 23
-        versionName '0.99.4'
-        minSdkVersion 23
-        targetSdkVersion 30
+        versionCode 25
+        versionName '0.99.5'
+        minSdkVersion 26
+        targetSdkVersion 33
         testInstrumentationRunner "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
@@ -35,6 +37,11 @@ android {
         sourceCompatibility JavaVersion.VERSION_1_8
         targetCompatibility JavaVersion.VERSION_1_8
     }
+    namespace 'org.ecmdroid'
+    buildFeatures {
+        buildConfig true
+    }
+    compileSdk 34
 }
 
 dependencies {
@@ -42,9 +49,9 @@ dependencies {
     implementation 'com.android.support:appcompat-v7:28.0.0'
     implementation 'com.android.support:design:28.0.0'
     implementation 'com.android.support.constraint:constraint-layout:1.1.3'
-    testImplementation 'junit:junit:4.12'
-    androidTestImplementation 'junit:junit:4.12'
-    androidTestImplementation 'com.android.support:support-v4:28.0.0'
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'com.android.support:support-v13:28.0.0'
     androidTestImplementation 'com.android.support.test:runner:1.0.2'
     androidTestImplementation 'com.android.support.test:rules:1.0.2'
 }

app/src/androidTest/java/org/ecmdroid/TestBin2Msl.java
@@ -17,16 +17,19 @@
  */
 package org.ecmdroid;
 
-import android.test.AndroidTestCase;
-
 import org.ecmdroid.util.Bin2MslConverter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class TestBin2Msl extends AndroidTestCase {
+@RunWith(JUnit4.class)
+public class TestBin2Msl{
 
+	@Test
 	public void testConversion() throws IOException {
 		ByteArrayInputStream bin = new ByteArrayInputStream(TestUtils.readBinaryLog());
 		ByteArrayOutputStream msl = new ByteArrayOutputStream(1024 * 1024 * 10);

app/src/androidTest/java/org/ecmdroid/TestBitSetProvider.java
@@ -17,23 +17,31 @@
  */
 package org.ecmdroid;
 
-import android.test.AndroidTestCase;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.ecmdroid.Constants.DataSource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 
+import static android.support.test.InstrumentationRegistry.getContext;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-public class TestBitSetProvider extends AndroidTestCase {
+
+@RunWith(AndroidJUnit4.class)
+public class TestBitSetProvider {
 	private BitSetProvider p;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		p = BitSetProvider.getInstance(this.getContext());
+	@Before
+	public void setUp() {
+		p = BitSetProvider.getInstance(getContext());
 	}
 
-	public void testBitSetConstructor() {
+	 @Test
+	 public void testBitSetConstructor() {
 		BitSet bits = p.getBitSet("BUEIB", "CDiag0", DataSource.RUNTIME_DATA);
 		assertNotNull(bits);
 		assertEquals("CDiag0", bits.getName());
@@ -45,6 +53,7 @@ public class TestBitSetProvider extends AndroidTestCase {
 		}
 	}
 
+	@Test
 	public void testFlags1() throws IOException {
 		byte[] data = TestUtils.readRTData();
 		BitSet bitset = p.getBitSet("BUEIB", "Flags1", DataSource.RUNTIME_DATA);

app/src/androidTest/java/org/ecmdroid/TestECM.java
@@ -17,29 +17,36 @@
  */
 package org.ecmdroid;
 
-import android.test.AndroidTestCase;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.ecmdroid.Error.ErrorType;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.Collection;
 
+import static android.support.test.InstrumentationRegistry.getContext;
+import static org.junit.Assert.assertEquals;
 
-public class TestECM extends AndroidTestCase {
+
+@RunWith(AndroidJUnit4.class)
+public class TestECM{
 	private ECM ecm;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+	@Before
+	public void setUp() throws Exception {
 		byte[] epd = TestUtils.readEEPROM();
 		byte[] rtd = TestUtils.readRTData();
 		ecm = ECM.getInstance(getContext());
 		EEPROM eeprom = EEPROM.get("BUEIB", getContext());
 		ecm.setEEPROM(eeprom);
-		ecm.getRuntimeData(rtd);
+		ecm.setRuntimeData(rtd);
 		System.arraycopy(epd, 0, eeprom.getBytes(), 0, epd.length);
 	}
 
+	@Test
 	public void testErrorParsing() throws IOException {
 		Collection<Error> errors;
 
@@ -52,10 +59,12 @@ public class TestECM extends AndroidTestCase {
 		assertEquals("21", (errors.iterator().next()).getCode());
 	}
 
+	@Test
 	public void testSerialNo() {
 		assertEquals("204", ecm.getSerialNo());
 	}
 
+	@Test
 	public void testMfgDate() {
 		ecm.getMfgDate();
 		assertEquals("6/8/06", ecm.getMfgDate());

app/src/androidTest/java/org/ecmdroid/TestEEPROM.java
@@ -17,10 +17,18 @@
  */
 package org.ecmdroid;
 
-import android.test.AndroidTestCase;
+import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class TestEEPROM extends AndroidTestCase {
+import static android.support.test.InstrumentationRegistry.getContext;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class TestEEPROM {
+	@Test
 	public void testVersion() {
 		EEPROM eeprom = EEPROM.get("BUEIB310 10-11-03", getContext());
 		assertNotNull(eeprom);

app/src/androidTest/java/org/ecmdroid/TestPDU.java
@@ -17,14 +17,24 @@
  */
 package org.ecmdroid;
 
-import android.test.AndroidTestCase;
+import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class TestPDU extends AndroidTestCase {
+
+@RunWith(JUnit4.class)
+public class TestPDU {
+	@Test
 	public void testVersionParsing() throws ParseException, UnsupportedEncodingException {
 		byte[] data = new byte[]{0x01, 0x42, 0x00, 0x13, (byte) 0xFF, 0x02, 0x06, 0x42, 0x55, 0x45, 0x49, 0x42, 0x33, 0x31, 0x30, 0x20, 0x31, 0x32, 0x2D, 0x31, 0x31, 0x2D, 0x30, 0x33, 0x03, (byte) 0xE2};
 		PDU pdu = new PDU(data, data.length);
@@ -35,6 +45,7 @@ public class TestPDU extends AndroidTestCase {
 		assertEquals("BUEIB310 12-11-03", new String(pdu.getEEPromData(), "US-ASCII"));
 	}
 
+	@Test
 	public void testSetRequest() throws UnsupportedEncodingException {
 		byte[] payload = "TestMe".getBytes();
 		PDU pdu = PDU.setRequest(0x42, 0x24, payload, 0, payload.length);
@@ -47,6 +58,7 @@ public class TestPDU extends AndroidTestCase {
 		assertEquals("TestMe", new String(pdu.getEEPromData(), "US-ASCII"));
 	}
 
+	@Test
 	public void testGetRequest() {
 		PDU pdu = PDU.getRequest(1, 0, 0x10);
 		assertTrue(pdu.isRequest());
@@ -57,6 +69,7 @@ public class TestPDU extends AndroidTestCase {
 		assertEquals("01:00:42:05:ff:02:52:00:01:10:03:fa", Utils.hexdump(pdu.getBytes()).toLowerCase());
 	}
 
+	@Test
 	public void testGetResponse() throws ParseException {
 		byte[] data = new byte[]{0x01, 0x42, 0x00, 0x12, (byte) 0xff, 0x02, 0x06, 0x00, 0x00, 0x00, 0x01, 0x00, 0x4d, (byte) 0xe8, 0x03, 0x7b, 0x06, (byte) 0x9e, 0x00, (byte) 0xcc, 0x00, (byte) 0xf4, 0x01, 0x03, (byte) 0xd5};
 		PDU pdu = new PDU(data, data.length);
@@ -67,6 +80,7 @@ public class TestPDU extends AndroidTestCase {
 		assertEquals("00:00:00:01:00:4d:e8:03:7b:06:9e:00:cc:00:f4:01", Utils.hexdump(pdu.getEEPromData()).toLowerCase());
 	}
 
+	@Test
 	public void testFunctions() {
 		PDU pdu = PDU.commandRequest(PDU.Function.Fan);
 		Log.d("TestPDU", pdu.toString());

app/src/androidTest/java/org/ecmdroid/TestVariableProvider.java
@@ -17,23 +17,34 @@
  */
 package org.ecmdroid;
 
-import android.test.AndroidTestCase;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.ecmdroid.Variable.DataType;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.Collection;
 
+import static android.support.test.InstrumentationRegistry.getContext;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-public class TestVariableProvider extends AndroidTestCase {
+
+@RunWith(AndroidJUnit4.class)
+public class TestVariableProvider{
 	private VariableProvider provider;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		provider = VariableProvider.getInstance(this.getContext());
+	@Before
+	public void setUp() throws Exception {
+		provider = VariableProvider.getInstance(getContext());
 	}
 
+	@Test
 	public void testRtVariableNames() {
 		Collection<String> vars = provider.getRtVariableNames("BUE2D");
 		assertEquals(111, vars.size());
@@ -42,6 +53,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		assertTrue(vars.contains("CLT"));
 	}
 
+	@Test
 	public void testScalarRtVariableNames() {
 		Collection<String> vars = provider.getScalarRtVariableNames("BUE2D");
 		assertEquals(79, vars.size());
@@ -50,6 +62,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		assertTrue(vars.contains("EGO1 Corr."));
 	}
 
+	@Test
 	public void testBitfieldRtVariableNames() {
 		Collection<String> vars = provider.getBitfieldRtVariableNames("BUE2D");
 		assertEquals(32, vars.size());
@@ -58,6 +71,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		assertTrue(vars.contains("DOut2 Feedb."));
 	}
 
+	@Test
 	public void testRtVariable() {
 		Variable v = provider.getRtVariable("BUE2D", "pw2");
 		assertNotNull(v);
@@ -77,6 +91,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		assertEquals(65535, v.getUhigh());
 	}
 
+	@Test
 	public void testRTParsing() throws IOException {
 		byte[] data = TestUtils.readRTData();
 		Variable v = provider.getRtVariable("BUEIB", "AFV");
@@ -85,6 +100,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		assertEquals("100.0%", v.getFormattedValue());
 	}
 
+	@Test
 	public void testAllRTVariables() throws IOException {
 		byte[] data = TestUtils.readRTData();
 		Collection<String> vars = provider.getRtVariableNames("BUEIB");
@@ -96,6 +112,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		}
 	}
 
+	@Test
 	public void testEEPROMVariable() throws IOException {
 		byte[] eeprom = TestUtils.readEEPROM();
 		Variable var = provider.getEEPROMVariable("BUEIB", "Country_ID");
@@ -124,6 +141,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		assertEquals((byte) 0xDC, eeprom[var.getOffset()]);
 	}
 
+	@Test
 	public void testNameLookup() {
 		assertNull(provider.getName("DOES_NOT_EXIST"));
 		assertEquals("System Configuration", provider.getName("KConfig"));
@@ -131,6 +149,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		assertNull(provider.getName("KConfig[10]"));
 	}
 
+	@Test
 	public void testAxis() throws IOException {
 		byte[] eeprom = TestUtils.readEEPROM();
 		Variable var = provider.getEEPROMVariable("BUEIB", "Tab_Fuel_Load_Ax");
@@ -172,6 +191,7 @@ public class TestVariableProvider extends AndroidTestCase {
 		}
 	}
 
+	@Test
 	public void testTable() throws IOException {
 		byte[] eeprom = TestUtils.readEEPROM();
 		Variable var = provider.getEEPROMVariable("BUEIB", "Tab_ABP_Conv");

commit cf89359f3a8a29458bd327f36d3c4d5690a2f1c5

log-recorder: Switch back logfile suffix to '.bin'
Commit d60ef87 accidentally changed the logfile suffix to '.log'.

This caused some confusion for veteran users of the app.

app/src/main/java/org/ecmdroid/fragments/LogFragment.java
@@ -239,7 +239,7 @@ public class LogFragment extends Fragment implements OnClickListener {
 
 		docRoot = DocumentFile.fromTreeUri(getContext(), Uri.parse(uri));
 		logTimestamp = DateFormat.format("yyyyMMdd_kkmmss", System.currentTimeMillis());
-		DocumentFile f = docRoot.createFile("application/x-ecmdroid-binlog", logTimestamp.toString() + ".log");
+		DocumentFile f = docRoot.createFile("application/x-ecmdroid-binlog", logTimestamp.toString() + ".bin");
 		logFile = getContext().getContentResolver().openFileDescriptor(f.getUri(), "rw");
 		FileOutputStream out = new FileOutputStream(logFile.getFileDescriptor());
 